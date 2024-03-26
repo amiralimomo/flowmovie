bc081a9ad5038f3c6d81ebcdcf28861e6f6e0f9a
amiralimomo <amiralimomo77@gmail.com>
2024-03-26 08:56:14 +0330

    add font , net step create slider for main page

--- src/shared/genre.js
+++ src/shared/genre.js
@@ -10,6 +10,9 @@ export const Genre = () => {
     }, [])
     return (
         <div className="genre-body">
+            <div className="genre-title-parent">
+                <span className="genre-title">Movies Genre</span>
+            </div>
             <ul className="d-flex flex-wrap">
 
                 { genre.map((item)=>{return( <li key={item.id} className="gener-li" ><a className="gener-a" href="">{item.name}</a></li>)})}
