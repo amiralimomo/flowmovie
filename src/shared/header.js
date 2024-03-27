5e9876454371e74322c6bc8e7f944e264083ec31
amiralimomo <amiralimomo77@gmail.com>
2024-03-27 13:26:59 +0330

    add container to header

--- src/shared/header.js
+++ src/shared/header.js
@@ -13,33 +13,34 @@ export const Header = () => {
         } else {
             setHover(true)
         }
-       
+
     }
     return (
-        <header className="header d-flex justify-content-around" >
-            <div className="d-flex justify-content-start">
-                <div><img className="header-logo" src={logo} alt="logo" /></div>
-                <ul className="d-flex justify-content-start align-items-stretch header-ul">
-                    <li className="d-flex justify-content-center align-items-center header-li"><a className="d-flex justify-content-center align-items-center header-li header-a" href="/">Home</a> </li>
-                    <li className="d-flex justify-content-center align-items-center header-li"><span onMouseEnter={() => onHandleHover()} onMouseLeave={() => onHandleHover()} className="d-flex justify-content-center align-items-center header-li header-a" href="/">Movies      <div className={hover ? "d-block trans" : "d-none"} style={{}}>
-                <Genre />
+        <header className="header " >
+            <div className='header-child d-flex justify-content-around container'>
 
-            </div></span>  <i className="bi bi-chevron-down header-link-icon"></i></li>
-                    <li className="d-flex justify-content-center align-items-center header-li"><a className="d-flex justify-content-center align-items-center header-li header-a" href="/">About</a> </li>
-                </ul>
+
+                <div className="d-flex justify-content-start">
+                    <div><img className="header-logo" src={logo} alt="logo" /></div>
+                    <ul className="d-flex justify-content-start align-items-stretch header-ul">
+                        <li className="d-flex justify-content-center align-items-center header-li"><a className="d-flex justify-content-center align-items-center header-li header-a" href="/">Home</a> </li>
+                        <li className="d-flex justify-content-center align-items-center header-li"><span onMouseEnter={() => onHandleHover()} onMouseLeave={() => onHandleHover()} className="d-flex justify-content-center align-items-center header-li header-a" href="/">Movies      <div className={hover ? "d-block trans" : "d-none"} style={{}}>
+                            <Genre />
+
+                        </div></span>  <i className="bi bi-chevron-down header-link-icon"></i></li>
+                        <li className="d-flex justify-content-center align-items-center header-li"><a className="d-flex justify-content-center align-items-center header-li header-a" href="/">About</a> </li>
+                    </ul>
+                </div>
+                <div className="d-flex justify-content-center align-items-center " >
+
+                    <form className='header-search-form '>
+                        <input className='header-search ' placeholder='Search...' type="text" />
+                        <button className='header-search-button'><img className='header-form-icon' src={search} alt="" /></button>
+
+                    </form>
+                </div>
+
             </div>
-            <div className="d-flex justify-content-center align-items-center " >
-
-                <form className='header-search-form '>
-                    <input className='header-search ' placeholder='Search...' type="text" />
-                    <button className='header-search-button'><img className='header-form-icon' src={search} alt="" /></button>
-
-                </form>
-            </div>
-            {/* <div className={hover ? "d-block trans" : "d-none"} style={{}}>
-                <Genre />
-
-            </div> */}
         </header>
     )
 }   
